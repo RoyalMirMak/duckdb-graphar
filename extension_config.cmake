4831719c20433c5744f3d53ecfa9558871ad2b73
Махлин Мирон Юрьевич <miron.makhlin@gmail.com>
2026-06-13 01:04:47 +0300

    bulid fix

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,8 +1,17 @@
 # Configuration for the DuckDB 'duckdb_graphar' extension.
-#
+
+# Include dependency paths set by Makefile (ARROW_ROOT, GRAPHAR_ROOT)
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/third_party/extension_deps.cmake")
+    include("${CMAKE_CURRENT_LIST_DIR}/third_party/extension_deps.cmake")
+endif()
+
+# Set include paths with GraphAr and Arrow
+set(GRAPHAR_INCLUDE_PATH "${GRAPHAR_ROOT}/include")
+set(ARROW_INCLUDE_PATH "${ARROW_ROOT}/include")
+set(EXTENSION_INCLUDE_PATH "${CMAKE_CURRENT_LIST_DIR}/include")
 
 duckdb_extension_load(duckdb_graphar
-    INCLUDE_DIR "${EXTENSION_ROOT_DIR}/include"
+    INCLUDE_DIR "${GRAPHAR_INCLUDE_PATH};${ARROW_INCLUDE_PATH};${EXTENSION_INCLUDE_PATH}"
     SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}"
     LOAD_TESTS
 )
