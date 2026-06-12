ba841cb997975e592515692767998a5b6d31a935
Махлин Мирон Юрьевич <miron.makhlin@gmail.com>
2026-06-13 02:14:57 +0300

    bulid fix

--- extension_config.cmake
+++ extension_config.cmake
@@ -10,8 +10,10 @@ set(GRAPHAR_INCLUDE_PATH "${GRAPHAR_ROOT}/include")
 set(ARROW_INCLUDE_PATH "${ARROW_ROOT}/include")
 set(EXTENSION_INCLUDE_PATH "${CMAKE_CURRENT_LIST_DIR}/include")
 
+# Register the extension with DuckDB's build system
+# This sets DUCKDB_EXTENSION_DUCKDB_GRAPHAR_INCLUDE_PATH which is used by the build system
 duckdb_extension_load(duckdb_graphar
-    INCLUDE_DIR "${GRAPHAR_INCLUDE_PATH};${ARROW_INCLUDE_PATH};${EXTENSION_INCLUDE_PATH}"
+    INCLUDE_DIR "${EXTENSION_INCLUDE_PATH}"
     SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}"
     LOAD_TESTS
 )
